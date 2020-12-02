f49f4871d038fc9304f1df67d37fd31f03de2bc9
Wesley Vos <17592840+Wesley-Vos@users.noreply.github.com>
2020-12-02 14:45:49 +0100

    Beautified day1

--- Day1/day1.py
+++ Day1/day1.py
@@ -2,17 +2,13 @@ import itertools
 import math
 
 input_file = open("input.txt", "r")
-input_data = []
-for line in input_file.readlines():
-    input_data.append(int(line.strip()))
+input_data = list(map(lambda x: int(x), input_file.read().splitlines()))
 input_file.close()
-
 
 def find_sums(n):
     for combination in itertools.combinations(input_data, n):
-        if sum(list(combination)) == 2020:
-            print(math.prod(list(combination)))
-
+        if sum(combination) == 2020:
+            print(math.prod(combination))
 
 find_sums(2)
 find_sums(3)

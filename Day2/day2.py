e9ba8571278b968d5d630d5c395204fad592b620
Wesley Vos <17592840+Wesley-Vos@users.noreply.github.com>
2020-12-03 11:40:35 +0100

    Beautified day2

--- Day2/day2.py
+++ Day2/day2.py
@@ -5,15 +5,15 @@ def valid_pass(part, first, second, criteria, password):
     if part == 1:
         result = sum(letter == criteria for letter in password)
         return (int(first) <= result <= int(second))
-    else:
-        return bool(password[int(first) - 1] == criteria) != bool(password[int(second) - 1] == criteria)
+    elif part == 2:
+        return (password[int(first) - 1] == criteria) ^ (password[int(second) - 1] == criteria)
 
 
 def main():
     with open("input.txt", "r") as file:
         data = file.read().splitlines()
-    data = list(map(lambda x: re.match(
-        "([0-9]*)-([0-9]*) ([^:]+): ([^\n]+)", x).groups(), data))
+    pattern = "([0-9]*)-([0-9]*) ([^:]+): ([^\n]+)"
+    data = list(map(lambda in_str: re.match(pattern, in_str).groups(), data))
 
     part_1 = sum(valid_pass(1, *d) for d in data)
     part_2 = sum(valid_pass(2, *d) for d in data)

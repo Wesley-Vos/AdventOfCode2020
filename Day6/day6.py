366af4f1b12e2848a21929c265fa6b89bd2f55cd
root <root@h2809510.stratoserver.net>
2020-12-08 13:43:19 +0100

    Added Day6

---
+++ Day6/day6.py
@@ -0,0 +1,24 @@
+def main():
+    with open("input.txt") as f:
+        data = f.read().split("\n\n")
+
+    counts = 0
+    for group in data:
+        persons = group.split("\n")
+        persons = [person for person in persons if person != ""]
+        print(persons)
+        count = [0 for _ in range(0, 26)]
+        for person in persons:
+            for letter in person:
+                count[ord(letter) - ord('a')] += 1
+        print(count)
+        print(len(persons))
+        for el in count:
+            if(el == len(persons)):
+                print("Ja", el, len(persons))
+            counts += (el == len(persons))
+    
+    print(counts)
+
+if __name__ == "__main__":
+    main()

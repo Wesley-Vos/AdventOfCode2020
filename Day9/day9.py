a2c96ad9a1afe607cddef907032af5e5706b0c80
Wesley Vos <17592840+Wesley-Vos@users.noreply.github.com>
2020-12-09 10:17:07 +0100

    Day 9 finished

---
+++ Day9/day9.py
@@ -0,0 +1,37 @@
+import itertools
+
+
+def find_invalid_number(preamble, n):
+    for i in range(n, len(preamble)):
+        number = preamble[i]
+        combinations = list(itertools.combinations(preamble[(i-n):i], 2))
+        sums = [sum(combination) for combination in combinations]
+        if number not in sums:
+            return number
+
+
+def find_contiguous_set(preamble, invalid_number):
+    for start in range(0, len(preamble) - 1):
+        end = start
+        set_sum = 0
+        while set_sum < invalid_number:
+            set_sum += preamble[end]
+            end += 1
+
+        if set_sum == invalid_number and (end - start) >= 2:
+            return min(preamble[start:end]) + max(preamble[start:end])
+    return "No contiguous set found"
+
+
+def main():
+    with open("input.txt") as f:
+        preamble = list(map(int, f.read().splitlines()))
+
+    invalid_number = find_invalid_number(preamble, 25)
+    print("Part 1:", invalid_number)
+    part2 = find_contiguous_set(preamble, invalid_number)
+    print("Part 2:", part2)
+
+
+if __name__ == "__main__":
+    main()

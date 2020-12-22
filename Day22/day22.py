71757bd2ce93f269e4770905b325a5f60ac39b81
Wesley Vos <Wesley-Vos@users.noreply.github.com>
2020-12-22 18:29:07 +0100

    Finished day22

--- Day22/day22.py
+++ Day22/day22.py
@@ -1,85 +1,60 @@
 def change_decks(deck_win, deck_lose):
-    deck_win.append(deck_win[0])
-    deck_win.append(deck_lose[0])
+    deck_win.extend([deck_win[0], deck_lose[0]])
     deck_win.pop(0)
     deck_lose.pop(0)
 
     return deck_win, deck_lose
 
+def calc_score(deck1, deck2):
+    deck = deck1 if len(deck2) == 0 else deck2
+    return sum([card*(len(deck) - i) for i, card in enumerate(deck)])
 
-def part1(deck1, deck2):
+def play_combat(deck1, deck2):
     while len(deck1) > 0 and len(deck2) > 0:
         if deck1[0] > deck2[0]:
             deck1, deck2 = change_decks(deck1, deck2)
         else:
-            deck1, deck2 = change_decks(deck2, deck1)
+            deck2, deck1 = change_decks(deck2, deck1)
 
-    deck = deck1 if len(deck1) > 0 else deck2
-    return sum([card*(len(deck) - i) for i, card in enumerate(deck)])
+    return deck1, deck2
 
-
-def game(deck1, deck2, sub=False):
-    print("New game started")
-    history_player1 = []
-    history_player2 = []
-    round = 1
+def play_recursive_combat(deck1, deck2, sub=False):
+    mem_deck1 = []
+    mem_deck2 = []
 
     while len(deck1) > 0 and len(deck2) > 0:
-        print("Round", round)
-        print("Deck 1:", deck1)
-        print("Deck 2:", deck2)
+        if deck1 in mem_deck1 or deck2 in mem_deck2:
+            return 1
+        mem_deck1.append(deck1.copy())
+        mem_deck2.append(deck2.copy())
+         
+        if len(deck1)-1 >= deck1[0] and len(deck2)-1 >= deck2[0]:
+            winner = play_recursive_combat(deck1[1:(deck1[0] + 1)],
+                          deck2[1:(deck2[0] + 1)], sub=True)
+        else:
+            winner = 1 if deck1[0] > deck2[0] else 2
 
-        if deck1 in history_player1 or deck2 in history_player2:
-            print(history_player1 if deck1 in history_player1 else history_player2)
-            print("Player 1 wins, found in history deck",
-                  1 if deck1 in history_player1 else 2)
-
-            history_player1.append(deck1)
-            history_player2.append(deck2)
-
+        if winner == 1:
             deck1, deck2 = change_decks(deck1, deck2)
         else:
-            history_player1.append(deck1)
-            history_player2.append(deck2)
+            deck2, deck1 = change_decks(deck2, deck1)
 
-        if (len(deck1) - 1) >= deck1[0] and (len(deck2) - 1) <= deck2[0]:
-            winner = game(deck1[1:(deck1[0] + 1)],
-                          deck2[1:(deck2[0] + 1)], sub=True)
-            if winner == 1:
-                deck1, deck2 = change_decks(deck1, deck2)
-            else:
-                deck1, deck2 = change_decks(deck1, deck2)
-            break
-
-        else:
-            if deck1[0] > deck2[0]:
-                print("Player 1 wins (higer card)")
-                deck1, deck2 = change_decks(deck1, deck2)
-            else:
-                print("Player 2 wins (higher card)")
-                deck1, deck2 = change_decks(deck2, deck1)
-
-        # history_player1.append(deck1)
-        # history_player2.append(deck2)
-        round += 1
 
     if sub:
-        print("Player", 1 if len(deck2) == 0 else 2, "won the game, go back")
         return 1 if len(deck2) == 0 else 2
     else:
-        deck = deck1 if len(deck1) > 0 else deck2
-        return sum([card*(len(deck) - i) for i, card in enumerate(deck)])
+        return deck1, deck2
 
 
 def main():
-    with open("test_input.txt") as f:
+    with open("input.txt") as f:
         raw = f.read().split("\n\n")
 
     deck1 = list(map(int, raw[0].splitlines()[1:]))
     deck2 = list(map(int, raw[1].splitlines()[1:]))
 
-    #print("Part 1:", part1(deck1, deck2))
-    print("Part 2:", game(deck1, deck2))
+    print("Part 1:", calc_score(*play_combat(deck1.copy(), deck2.copy())))
+    print("Part 2:", calc_score(*play_recursive_combat(deck1.copy(), deck2.copy())))
 
 
 if __name__ == "__main__":

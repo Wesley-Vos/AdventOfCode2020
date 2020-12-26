911a8ecacc2411f5f0a0cc7a034b4f8f21647487
Wesley Vos <Wesley-Vos@users.noreply.github.com>
2020-12-26 13:54:45 +0100

    19

--- Day19/day19.py
+++ Day19/day19.py
@@ -1,24 +1,42 @@
 import re
 from collections import defaultdict
 
+def recurse_rule(idx, rules):
+    rule = rules[idx]
+    # print("Recurse", idx)
+    for i, dis in enumerate(rule):
+        for j, con in enumerate(dis):
+            if isinstance(con, int):
+                result = recurse_rule(con, rules)
+                print(result)
+                print("")
+                rule[i][j] = result
+            elif isinstance(con, str):
+                rule[i][j] = con
+                return rule
+                
+    return rules
         
 def main():
     with open("input.txt") as f:
-        data = f.read().splitlines()
+        raw_data = f.read().splitlines()
         
-    
     rules = defaultdict()
-    for rule in data[0:131]:
-        rule = rule.split(": ")
-        rules[int(rule[0])] = rule[1]
+    for i, rule in enumerate(raw_data[0:131]):
+        raw_rule = rule.split(": ")
+        rule = []
+        for r in raw_rule[1].split(" | "):
+            try:
+                rule.append(list(map(int, r.split(" "))))
+            except:
+                rule.append(list(map(lambda x: x[1:2], r.split(" "))))
+        rules[int(raw_rule[0])] = rule
     
-    result = rule[0]
-    if isinstance(result, int):
-        result = 
+    rules = recurse_rule(0, rules)
+    # print(rules[0])
     
-        
-
-    messages = data[132:]
+    
+    messages = raw_data[132:]
     
 
 if __name__ == "__main__":

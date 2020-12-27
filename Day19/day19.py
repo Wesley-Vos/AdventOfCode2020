6eee6f3e884aa03d8c8c501b7ddad315c4f9fe91
Wesley Vos <Wesley-Vos@users.noreply.github.com>
2020-12-27 10:22:15 +0100

    next

--- Day19/day19.py
+++ Day19/day19.py
@@ -3,23 +3,34 @@ from collections import defaultdict
 
 def recurse_rule(idx, rules):
     rule = rules[idx]
+    new_rules = []
     # print("Recurse", idx)
     for i, dis in enumerate(rule):
         for j, con in enumerate(dis):
             if isinstance(con, int):
                 result = recurse_rule(con, rules)
-                print(result)
-                print("")
+                # print(result)
+                #print("")
                 rule[i][j] = result
             elif isinstance(con, str):
                 rule[i][j] = con
-                return rule
-                
-    return rules
+                return con
+        #print(rule[i])
+        new_rules.append("".join(rule[i]))
+    
+    #string = ""
+    #for rule in new_rules:
+        #string += "(?:" + rule ")" + "|"
+    if len(new_rules) == 1:
+        return new_rules[0]
+    else:
+        return "(?:" + new_rules[0] + "|" + new_rules[1] + ")"
+    #return "^(?:" + ")|(?:".join(new_rules) + ")$"
         
 def main():
     with open("input.txt") as f:
-        raw_data = f.read().splitlines()
+        raw_data, messages = f.read().split("\n\n")
+        raw_data = raw_data.splitlines()
         
     rules = defaultdict()
     for i, rule in enumerate(raw_data[0:131]):
@@ -31,13 +42,14 @@ def main():
             except:
                 rule.append(list(map(lambda x: x[1:2], r.split(" "))))
         rules[int(raw_rule[0])] = rule
+    #print((rules[0][0]))
+    rules[0] = recurse_rule(0, rules)
+    #print(rules[0])
+    print(type(rules[0]))
     
-    rules = recurse_rule(0, rules)
-    # print(rules[0])
     
-    
-    messages = raw_data[132:]
-    
+    ree = re.compile(r"^" + rules[0] + "$")
+    print(sum(bool(ree.match(x)) for x in messages))
 
 if __name__ == "__main__":
     main()
